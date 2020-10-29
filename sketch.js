848c8ab092be97233d49c096bc80b73e2eb5d876
OwaisKhan02 <72199555+OwaisKhan02@users.noreply.github.com>
2020-10-29 13:40:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var ground;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255) 
+	
+	engine = Engine.create();
+	world = engine.world;
+
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground).
+
+	
+	 Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
